f4af1215ca887a9a22f2d8034c25d038c0901be7
Seya <31957516+sglkc@users.noreply.github.com>
2025-05-23 14:08:13 +0700

    chal-1: hello world

---
+++ chal-1/go.mod
@@ -0,0 +1,3 @@
+module roketin-be-test/chal-1
+
+go 1.24.2
---
+++ chal-1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This is a simple program that prints "Hello, World!" to the console.
+	fmt.Println("Hello, World!")
+}
